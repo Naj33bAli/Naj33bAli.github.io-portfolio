8fbe0464d8b2a7cf41a5b6b6ec5d18955e06a573
Najeeb <Najeebmohali@gmail.com>
2024-11-23 18:36:49 -0600

    Switch to HashRouter for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import Portfolio from './components/portfolio';
 import TaskManager from './components/taskmanager';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        {/* Navigation links */}
-        <nav className="bg-gray-800 text-white p-4">
-       <ul className="text-center">
-      <li className="inline-block mx-4">
-       <Link to="/" className="hover:text-gray-400">Portfolio</Link>
-        </li>
-        <li className="inline-block mx-4">
-      <Link to="/task-manager" className="hover:text-gray-400"></Link>
+    <div className="App">
+      {/* Navigation links */}
+      <nav className="bg-gray-800 text-white p-4">
+        <ul className="text-center">
+          <li className="inline-block mx-4">
+            <Link to="/" className="hover:text-gray-400">Portfolio</Link>
           </li>
-         </ul>
+          <li className="inline-block mx-4">
+            <Link to="/task-manager" className="hover:text-gray-400">Task Manager</Link>
+          </li>
+        </ul>
       </nav>
 
-        {/* Routes for different pages */}
-        <Routes>
-          <Route path="/" element={<Portfolio />} />
-          <Route path="/task-manager" element={<TaskManager />} />
-        </Routes>
-      </div>
-    </Router>
+      {/* Routes for different pages */}
+      <Routes>
+        <Route path="/" element={<Portfolio />} />
+        <Route path="/task-manager" element={<TaskManager />} />
+      </Routes>
+    </div>
   );
 }
 
